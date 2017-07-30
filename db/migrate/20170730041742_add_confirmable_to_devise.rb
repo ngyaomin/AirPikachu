eeefe41f1f1ba1f3fdcf01b273200479f94810a3
Yao Min <ngyaomin@ymail.com>
2017-07-30 12:28:20 +0800

    More

--- db/migrate/20170730041742_add_confirmable_to_devise.rb
+++ db/migrate/20170730041742_add_confirmable_to_devise.rb
@@ -5,5 +5,6 @@ class AddConfirmableToDevise < ActiveRecord::Migration[5.0]
     add_column :users, :confirmation_sent_at, :datetime
 
     add_index :users, :confirmation_token, unique: true
+    
   end
 end
