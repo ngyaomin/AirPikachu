dd00f5c8c7ca6c34b2cd8df516dffee6ace51b02
Yao Min <ngyaomin@ymail.com>
2017-07-30 21:01:19 +0800

    Creating and styling user profile

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,5 @@ Rails.application.routes.draw do
               path_names: {sign_in: 'login', sign_out: 'logout', edit: 'profile', sign_up: 'registration'},
               controllers: {omniauth_callbacks: 'omniauth_callbacks'}
 
+  resources :users, only: [:show]
 end
