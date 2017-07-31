0664156441d21afebc961a9b056ede3113c4ac1d
Yao Min <ngyaomin@ymail.com>
2017-07-31 19:32:31 +0800

    Define new and create functions

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -6,9 +6,11 @@ class RoomController < ApplicationController
   end
 
   def new
+    @room = current_user.rooms.build
   end
 
   def create
+    @room = current_user.rooms.build(room_params)
   end
 
   def listing
@@ -35,5 +37,11 @@ class RoomController < ApplicationController
   private
     def set_room
       @room = Room.find(params[:id])
+    end
+
+    def room_params
+      params.require(:room).permit(:home_type, :room_type, :accomodate, :bed_room, :bath_room,
+      :listing_name, :summary, :address, :is_tv, :is_kitchen, :is_air, :is_heating,
+      :is_internet, :price, :active)
     end
 end
