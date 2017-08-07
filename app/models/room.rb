34ce090a8d3be9702b4baae6a847c082a34ff0e3
Yao Min <ngyaomin@ymail.com>
2017-08-07 16:31:07 +0800

    Setting up google map continue

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,7 @@ class Room < ApplicationRecord
   has_many :photos
 
   geocoded_by :address
-  after_validation:  :geocode, if: :address_changed?
+  after_validation :geocode, if: :address_changed?
 
   validates :home_type, presence: true
   validates :room_type, presence: true
