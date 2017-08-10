a7b32a8d0cf90e136559ee979604f90f38190c4a
Yao Min <ngyaomin@ymail.com>
2017-08-10 21:33:04 +0800

    Prevent user from booking own room

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,19 +1,24 @@
 class ReservationsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     room = Room.find(params[:room_id])
 
-    start_date = Date.parse(reservation_params[:start_date])
-    end_date = Date.parse(reservation_params[:end_date])
-    days = (end_date - start_date).to_i + 1
+    if current_user == room.user
+      flash[:alert] = "You cannot book your own room"
+    else
+      start_date = Date.parse(reservation_params[:start_date])
+      end_date = Date.parse(reservation_params[:end_date])
+      days = (end_date - start_date).to_i + 1
 
-    @reservation = current_user.reservations.build(reservation_params)
-    @reservation.room = room
-    @reservation.price = room.price
-    @reservation.total = room.price * days
-    @reservation.save
+      @reservation = current_user.reservations.build(reservation_params)
+      @reservation.room = room
+      @reservation.price = room.price
+      @reservation.total = room.price * days
+      @reservation.save
 
-    flash[:notice] = "Success Booking"
+      flash[:notice] = "Success Booking"
+    end
     redirect_to room
   end
 
