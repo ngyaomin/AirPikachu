428faca8babbe553cb8dbccde4925915cebeb912
Yao Min <ngyaomin@ymail.com>
2017-08-10 20:50:37 +0800

    Create reservation model

---
+++ db/migrate/20170810125007_create_reservations.rb
@@ -0,0 +1,15 @@
+class CreateReservations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reservations do |t|
+      t.references :user, foreign_key: true
+      t.references :room, foreign_key: true
+      t.datetime :start_date
+      t.datetime :end_date
+      t.string :price
+      t.string :integer
+      t.integer :total
+
+      t.timestamps
+    end
+  end
+end
