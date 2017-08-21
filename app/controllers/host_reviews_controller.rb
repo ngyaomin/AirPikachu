7e35ea9574239e84144b95d8d8d547f083307f59
Yao Min <ngyaomin@ymail.com>
2017-08-21 11:00:14 +0800

    Do the create and destroy function for hostreview controller

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -1,5 +1,16 @@
 class HostReviewController < ApplicationController
 
+  def create
+    @host_review = current_user.host_reviews.create(host_review_params)
+    redirect_back(fallback_location: request.referer)
+  end
+
+  def destroy
+    @host_review = Review.find(params[:id])
+    @host_review.destroy
+
+    redirect_back(fallback_location: request.referer, notice: "Removed!")
+  end
 
   private
     def host_review_params
