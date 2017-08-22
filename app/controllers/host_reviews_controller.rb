e62bc8d0a5d71d93671c135105a2b6ccd7e8f10f
Yao Min <ngyaomin@ymail.com>
2017-08-22 21:28:26 +0800

    Check if reservation exist and if review is already done

--- app/controllers/host_reviews_controller.rb
+++ app/controllers/host_reviews_controller.rb
@@ -1,6 +1,24 @@
 class HostReviewController < ApplicationController
 
   def create
+
+    @reservation = Reservation.where(
+                    id: host_review_params[:reservation_id],
+                    room_id: host_review_params[:room_id],
+                    user_id: host_review_params[:guest_id]
+                   ).first
+
+    if !@reservation.nil?
+
+      @has_reviewed = HostReview.where(
+                        reservation_id: @reservation.id,
+                        guest_id: host_review_params[:guest_id]
+                      ).first
+
+    else
+      flash[:alert] = "Reservation not found"
+    end
+
     @host_review = current_user.host_reviews.create(host_review_params)
     redirect_back(fallback_location: request.referer)
   end
